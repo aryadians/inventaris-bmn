bb91177e974094ce1f40e4b74611929a5b3d1ef5
Arya Dian <aryadian003@gmail.com>
2026-02-14 12:37:02 +0700

    Format laporan DBR

--- resources/views/pdf/kib_ruangan.blade.php
+++ resources/views/pdf/kib_ruangan.blade.php
@@ -1,117 +1,178 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Kartu Inventaris Barang (KIB) - {{ $room->nama_ruangan }}</title>
+    <title>Daftar Barang Ruangan (DBR) - {{ $room->nama_ruangan }}</title>
     <style>
-        body { font-family: sans-serif; font-size: 11px; }
-        .header { text-align: center; margin-bottom: 20px; font-weight: bold; }
-        .sub-header { text-align: left; margin-bottom: 15px; }
-        table { width: 100%; border-collapse: collapse; margin-top: 10px; }
-        th, td { border: 1px solid black; padding: 5px; text-align: left; }
-        th { background-color: #f2f2f2; text-align: center; font-weight: bold; }
-        .footer { margin-top: 30px; width: 100%; }
-        .ttd { width: 30%; float: right; text-align: center; }
-        .ttd-kiri { width: 30%; float: left; text-align: center; }
-        .clear { clear: both; }
-        .badge-rusak { color: red; font-weight: bold; font-size: 10px; }
+        @page {
+            margin: 1cm;
+            size: a4 landscape;
+        }
+        body { 
+            font-family: Arial, Helvetica, sans-serif; 
+            font-size: 10px; 
+            line-height: 1.4;
+        }
+        .header { 
+            text-align: center; 
+            margin-bottom: 20px; 
+            text-transform: uppercase;
+        }
+        .header h2 { margin: 0; font-size: 14px; }
+        .header h3 { margin: 2px 0; font-size: 12px; }
+        .header h4 { margin: 0; font-size: 11px; font-weight: normal; }
+        
+        .info-table { 
+            width: 100%; 
+            margin-bottom: 10px; 
+            border: none;
+        }
+        .info-table td { 
+            border: none; 
+            padding: 1px 0; 
+        }
+
+        table.main-table { 
+            width: 100%; 
+            border-collapse: collapse; 
+            margin-top: 5px; 
+        }
+        table.main-table th, table.main-table td { 
+            border: 1px solid black; 
+            padding: 4px; 
+        }
+        table.main-table th { 
+            background-color: #f2f2f2; 
+            text-align: center; 
+            font-weight: bold;
+            text-transform: uppercase;
+            font-size: 9px;
+        }
+        
+        .center { text-align: center; }
+        .bold { font-weight: bold; }
+        
+        .footer { 
+            margin-top: 30px; 
+            width: 100%; 
+        }
+        .ttd-container {
+            width: 100%;
+        }
+        .ttd-box {
+            width: 40%;
+            display: inline-block;
+            text-align: center;
+            vertical-align: top;
+        }
+        .spacer {
+            width: 15%;
+            display: inline-block;
+        }
+        
+        .condition-legend {
+            margin-top: 10px;
+            font-size: 8px;
+            font-style: italic;
+        }
     </style>
 </head>
 <body>
     <div class="header">
-        <h2>DAFTAR BARANG RUANGAN (DBR)</h2>
-        <h3>KEMENTERIAN HUKUM DAN HAM RI</h3>
-        <h4>LAPAS KELAS IIB JOMBANG</h4>
+        <h4>KEMENTERIAN HUKUM DAN HAK ASASI MANUSIA RI</h4>
+        <h4>KANTOR WILAYAH JAWA TIMUR</h4>
+        <h2>LAPAS KELAS IIB JOMBANG</h2>
+        <h3>DAFTAR BARANG RUANGAN (DBR)</h3>
     </div>
 
-    <div class="sub-header">
-        <table>
-            <tr>
-                <td style="border:none; width: 15%;">Unit Kerja</td>
-                <td style="border:none; width: 1%;">:</td>
-                <td style="border:none; width: 84%;">Lapas Kelas IIB Jombang</td>
-            </tr>
-            <tr>
-                <td style="border:none;">Kode Ruangan</td>
-                <td style="border:none;">:</td>
-                <td style="border:none;">{{ $room->kode_ruangan }}</td>
-            </tr>
-            <tr>
-                <td style="border:none;">Nama Ruangan</td>
-                <td style="border:none;">:</td>
-                <td style="border:none;">{{ $room->nama_ruangan }}</td>
-            </tr>
-            <tr>
-                <td style="border:none;">Penanggung Jawab</td>
-                <td style="border:none;">:</td>
-                <td style="border:none;">{{ $room->penanggung_jawab }}</td>
-            </tr>
-        </table>
-    </div>
+    <table class="info-table">
+        <tr>
+            <td width="15%">Unit Kerja</td>
+            <td width="2%">:</td>
+            <td width="33%">Lapas Kelas IIB Jombang</td>
+            <td width="15%">Nama Ruangan</td>
+            <td width="2%">:</td>
+            <td width="33%">{{ $room->nama_ruangan }}</td>
+        </tr>
+        <tr>
+            <td>Kode Unit Kerja</td>
+            <td>:</td>
+            <td>013.04.05.405540</td> {{-- Contoh kode Lapas --}}
+            <td>Kode Ruangan</td>
+            <td>:</td>
+            <td>{{ $room->kode_ruangan }}</td>
+        </tr>
+        <tr>
+            <td>Tahun Anggaran</td>
+            <td>:</td>
+            <td>{{ date('Y') }}</td>
+            <td>Petugas Ruangan</td>
+            <td>:</td>
+            <td>{{ $room->penanggung_jawab }}</td>
+        </tr>
+    </table>
 
-    <table>
+    <table class="main-table">
         <thead>
             <tr>
-                <th width="5%">No.</th>
-                <th width="12%">Kode Barang</th>
+                <th width="3%">NO</th>
+                <th width="20%">NAMA BARANG</th>
+                <th width="15%">MERK / TYPE</th>
+                <th width="12%">KODE BARANG</th>
                 <th width="5%">NUP</th>
-                <th width="25%">Nama Barang</th>
-                <th width="15%">Merk / Tipe</th>
-                <th width="10%">Tahun</th>
-                <th width="10%">Kondisi</th>
-                <th width="18%">Keterangan</th>
+                <th width="6%">THN PEROL.</th>
+                <th width="5%">B</th>
+                <th width="5%">RR</th>
+                <th width="5%">RB</th>
+                <th>KETERANGAN</th>
             </tr>
         </thead>
         <tbody>
             @forelse($assets as $index => $asset)
             <tr>
-                <td style="text-align: center;">{{ $index + 1 }}</td>
-                <td style="text-align: center;">{{ $asset->kode_barang }}</td>
-                <td style="text-align: center;">{{ $asset->nup }}</td>
+                <td class="center">{{ $index + 1 }}</td>
                 <td>{{ $asset->nama_barang }}</td>
                 <td>{{ $asset->merk_type ?? '-' }}</td>
-                <td style="text-align: center;">{{ $asset->tanggal_perolehan ? \Carbon\Carbon::parse($asset->tanggal_perolehan)->format('Y') : '-' }}</td>
-                <td style="text-align: center;">
-                    @if($asset->kondisi == 'RUSAK_BERAT')
-                        <span class="badge-rusak">RUSAK BERAT</span>
-                    @elseif($asset->kondisi == 'RUSAK_RINGAN')
-                        RUSAK RINGAN
-                    @else
-                        BAIK
-                    @endif
-                </td>
-                <td>{{ $asset->status == 'DIPINJAM' ? 'Dipinjam oleh ' . ($asset->loans->last()->user->name ?? '-') : 'Ada di Ruangan' }}</td>
+                <td class="center">{{ $asset->kode_barang }}</td>
+                <td class="center">{{ $asset->nup }}</td>
+                <td class="center">{{ $asset->tanggal_perolehan ? \Carbon\Carbon::parse($asset->tanggal_perolehan)->format('Y') : '-' }}</td>
+                <td class="center">{{ $asset->kondisi == 'BAIK' ? 'X' : '' }}</td>
+                <td class="center">{{ $asset->kondisi == 'RUSAK_RINGAN' ? 'X' : '' }}</td>
+                <td class="center">{{ $asset->kondisi == 'RUSAK_BERAT' ? 'X' : '' }}</td>
+                <td>{{ $asset->status == 'DIPINJAM' ? 'Pinjam: ' . ($asset->loans->last()->user->name ?? '-') : '' }}</td>
             </tr>
             @empty
             <tr>
-                <td colspan="8" style="text-align: center; padding: 20px;">
-                    <i>Tidak ada barang di ruangan ini.</i>
-                </td>
+                <td colspan="10" class="center"><i>Belum ada data barang terdaftar di ruangan ini.</i></td>
             </tr>
             @endforelse
         </tbody>
     </table>
 
-    <div class="footer">
-        <div class="ttd-kiri">
-            <br>
-            Petugas Ruangan,
-            <br><br><br><br>
-            <strong>{{ $room->penanggung_jawab }}</strong>
-            <br>
-            NIP. ...........................
-        </div>
+    <div class="condition-legend">
+        Keterangan Kondisi: B (Baik), RR (Rusak Ringan), RB (Rusak Berat)
+    </div>
 
-        <div class="ttd">
-            Jombang, {{ $date }}
-            <br>
-            Mengetahui,<br>
-            Kepala Urusan Umum
-            <br><br><br><br>
-            <strong>...........................</strong>
-            <br>
-            NIP. ...........................
+    <div class="footer">
+        <div class="ttd-container">
+            <div class="ttd-box">
+                <br>
+                Mengetahui,<br>
+                Kepala Urusan Umum
+                <br><br><br><br><br>
+                <span class="bold">..........................................</span><br>
+                NIP. ..........................................
+            </div>
+            
+            <div class="spacer"></div>
+            
+            <div class="ttd-box">
+                Jombang, {{ $date }}<br>
+                Petugas Ruangan / Penanggung Jawab
+                <br><br><br><br><br>
+                <span class="bold">{{ $room->penanggung_jawab }}</span><br>
+                NIP. ..........................................
+            </div>
         </div>
-        <div class="clear"></div>
     </div>
 </body>
 </html>
